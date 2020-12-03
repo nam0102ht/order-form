032c1c71b3a0d100ba72d7ec41858f7bcec0edd9
nguyentrungnhatnam <nguyentrungnhatnam@gmail.com>
2020-12-03 21:44:53 +0700

    make it better:

--- components/body/login/FormRegister.js
+++ components/body/login/FormRegister.js
@@ -10,12 +10,13 @@ import { Button,
     TextField
 } from "@material-ui/core"
 import styled from "styled-components"
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { Visibility, VisibilityOff } from "@material-ui/icons"
 import CloseIcon from '@material-ui/icons/Close'
 import registerUser from "../../../utils/hooks/register"
 import { useRouter } from "next/router"
 import Alert from "@material-ui/lab/Alert"
+import Axios from "axios"
 
 
 
@@ -71,9 +72,19 @@ export default function FormRegister() {
     const [messRes, setMessRes] = useState()
     const [isShow, setIsShow] = useState(false)
     const [closeAlert, setCloseAlert] = useState(true)
+    const [invalid, setInValid] = useState({
+        username: false,
+        password: false,
+        repassword: false,
+        firstname: false,
+        lastname: false,
+        phoneNumber: false,
+        email: false
+    })
+    const [showButton, setShowButton] = useState(false)
 
     useEffect(async () => {
-        const data = await axios.get('/api/me')
+        const data = await Axios.get('/api/me')
         if(data.data.user) {
             router.push('/')
         }
@@ -81,10 +92,22 @@ export default function FormRegister() {
 
     const onChangeHandle = useCallback((event) => {
         let user = userForm
+        let inval = invalid
         const {name, value} = event.target
         user[name] = value
+        if(user[name]) inval[name] = true
+        else inval[name] = false
         setUserForm(user)
-    }, [userForm, setUserForm])
+        setInValid(inval)
+        if(inval.email 
+            && inval.firstname 
+            && inval.lastname 
+            && inval.phoneNumber 
+            && inval.repassword
+            && inval.password
+            && inval.username
+        ) setShowButton(true)
+    }, [userForm, invalid, setInValid, setUserForm, setShowButton])
 
     const handleClickShowPassword = () => {
         setValues({ showRePassword: !values.showRePassword })
@@ -122,6 +145,7 @@ export default function FormRegister() {
     return (<PaperCustom>
         <FormLoginCustom>
             <TextStyle
+                error={invalid.username}
                 name="username"
                 label="Username:"
                 multiline
@@ -137,6 +161,7 @@ export default function FormRegister() {
                     type={values.showPassword ? 'text' : 'password'}
                     value={userForm.password}
                     name="password"
+                    error={invalid.password}
                     onChange={event => onChangeHandle(event)}
                     rowsMax={4}
                     endAdornment={
@@ -159,6 +184,7 @@ export default function FormRegister() {
                     type={values.showRePassword ? 'text' : 'password'}
                     value={userForm.repassword}
                     name="repassword"
+                    error={invalid.repassword}
                     onChange={event => onChangeHandle(event)}
                     rowsMax={4}
                     endAdornment={
@@ -176,6 +202,7 @@ export default function FormRegister() {
             </FormControlCustom>
             <TextStyle
                 name="firstName"
+                error={invalid.firstname}
                 label="Firstname:"
                 multiline
                 placeholder="Firstname"
@@ -185,6 +212,7 @@ export default function FormRegister() {
             />
             <TextStyle
                 name="lastName"
+                error={invalid.lastname}
                 label="Lastname:"
                 multiline
                 placeholder="Lastname"
@@ -194,6 +222,7 @@ export default function FormRegister() {
             />
             <TextStyle
                 name="phoneNumber"
+                error={invalid.phoneNumber}
                 label="Phone:"
                 multiline
                 placeholder="Phone"
@@ -203,6 +232,7 @@ export default function FormRegister() {
             />
             <TextStyle
                 name="email"
+                error={invalid.email}
                 label="Email:"
                 multiline
                 placeholder="Email"
@@ -211,7 +241,10 @@ export default function FormRegister() {
                 onChange={event => onChangeHandle(event)}
             />
             <Link href="/login">If account is registered, please login</Link>
-            <ButtonCustom onClick={onClickHandle}>Register</ButtonCustom>
+            <ButtonCustom
+                disabled={!showButton}
+                onClick={onClickHandle}>Register
+            </ButtonCustom>
         </FormLoginCustom>
         { isShow ? <FadeCustom in={closeAlert}>
                         <Alert
